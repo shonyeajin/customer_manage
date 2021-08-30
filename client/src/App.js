46452ac2361c64c41244d847eb618426a035a883
손예진 <whitettong@naver.com>
2021-08-30 10:31:17 +0900

    complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
+import CustomerAdd from './components/CustomerAdd';
 import Customer from './components/Customer';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -7,26 +8,55 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Paper from '@material-ui/core/Paper';
+import { CircularProgress } from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';//css사용하기 위해서
+
+/*
+- component의 라이프 사이클 -
+1) constructor()
+2) componentWillMount()
+3) render()
+4) componentDidMount()
+
+props or state chnage event -> shouldComponentUpdate()
+*/
 
 const styles=theme=>({
   root:{
     width:'100%',
-    marginTop:theme.spacing.unit*3,
+    marginTop:theme.spacing.unit * 3,
     overflow:"auto"
   },
   table:{
     minWidth:1080
+  },
+  progress:{
+    margin:theme.spacing.unit * 2
   }
 })
 
 
 class App extends Component{
-  state={
-    customers:""
+
+  constructor(props){
+    super(props);
+      this.state={
+        customers:'',
+        completed:0
+      }
+  }
+  stateRefresh=()=>{
+    this.setState({
+      customers:'',
+      complete:0
+    });
+    this.callApi()
+    .then(res=>this.setState({customers:res}))
+    .catch(err=>console.log(err));
   }
   
   componentDidMount(){
+    this.timer=setInterval(this.progress,250);//0.02초 마다 progress함수가 실행되게
     this.callApi()
     .then(res=>this.setState({customers:res}))
     .catch(err=>console.log(err));
@@ -39,29 +69,41 @@ class App extends Component{
     return body;
   }
 
+  progress=()=>{
+    const {completed} =this.state;
+    this.setState({completed : completed >= 100 ? 0 : completed+10});
+  }
+
   render(){
     const {classes}=this.props;
     return (
-      <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {this.state.customers?
-          this.state.customers.map(c=><Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}></Customer>):
-          ''
-          }
-        </TableBody>
-      </Table>
-    </Paper>
+      <div>
+        <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers?
+            this.state.customers.map(c=><Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}></Customer>):
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}></CircularProgress>
+              </TableCell>
+            </TableRow>}
+          </TableBody>
+        </Table>
+      </Paper>
+      <CustomerAdd stateRefresh={this.stateRefresh}></CustomerAdd>
+      </div>
+
     )
 
   }
--- server.js
+++ server.js
@@ -2,29 +2,59 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const app =express();//생성자
 const port=5000;//서버의 포트는 5000번으로
+const fs=require('fs');
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//mysql 연동 부분
+const data=fs.readFileSync('./database.json');
+const conf=JSON.parse(data);
+const mysql=require('mysql2');
+
+const connection=mysql.createConnection({
+    host:conf.host,
+    user:conf.user,
+    password:conf.password,
+    port:conf.port,
+    database:conf.database
+})
+
+connection.connect();
+
+//파일이 포함된것을 업로드 하려면 다음 라이브러리를 활용한 작업이 팔요함.
+//npm install --save multer
+const multer=require('multer');
+const upload=multer({dest:'./upload'});//업로드가 되는 위치 설정, 여기서는 루트폴더의 upload파일
+
 
 app.get('/api/customers',(req,res)=>{
-    res.send([{
-        'id': 1,
-        'image':'https://placeimg.com/64/64/1',
-        'name':'홍길동',
-        'birthday':'324',
-        'gender':'남자',
-        'job':'프로그래머'
-      },{
-        'id': 2,
-        'image':'https://placeimg.com/64/64/2',
-        'name':'김또또',
-        'birthday':'323244',
-        'gender':'중성화',
-        'job':'강아지'
-      },{
-        'id': 3,
-        'image':'https://placeimg.com/64/64/3',
-        'name':'손예진',
-        'birthday':'324222',
-        'gender':'남자',
-        'job':'학생'
-      }]);
-});
+    connection.query('select * from customer',(err,rows,fields)=>{
+        if (err) throw err;
+        res.send(rows);  
+    })
+})
+
+app.use('/image',express.static('./upload'));//사용자는 image라는 경로로 접근하는데 사실은 그게 upload경로에 접근하는거.그리고 express.static은 정적 자사이 포함된 디렉토리 이름을 넣어주면 이를 미들웨어 함수에 전달하여 파일의 직접적인 제공을 시작할 수 있음
+
+
+// app.get('/api/customers',(req,res)=>{
+//     res.send();
+// });
+
+app.post('/api/customers',upload.single('image'),(req,res)=>{
+    let sql='insert into customer values (null,?,?,?,?,?)';
+    let image='/image/'+req.file.filename;//multer가 알아서 안 겹치게 넣어줌
+    let name=req.body.name;
+    let birthday=req.body.birthday;
+    let gender=req.body.gender;
+    let job=req.body.job;
+    let params=[image, name, birthday, gender, job];
+    connection.query(sql, params, (err, rows, fields)=>{
+        if (err) throw err;
+        res.send(rows);
+    })
+})
 app.listen(port,()=>console.log(`Listening on port${port}`));
